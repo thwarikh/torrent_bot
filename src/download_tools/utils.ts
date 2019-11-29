1534132083923958a65f875b6bf5e0e2bb48ef82
Thwarikh Anvar T S <48545060+thwarikh@users.noreply.github.com>
2019-11-29 11:44:03 +0530

    Update utils.ts

--- src/download_tools/utils.ts
+++ src/download_tools/utils.ts
@@ -146,7 +146,7 @@ export function generateStatusMessage(totalLength: number, completedLength: numb
   var progressString = generateProgress(progress);
   var speedStr = formatSize(speed);
   var eta = downloadETA(totalLength, completedLength, speed);
-  var user = dlManager(username);
+  var user = details(tgUsername);
   var type = isUploading ? '📤Uploading' : '📥Downloading';
   var message = `<b>${type}</b>: <code>${fileName}</code>\n<b>Progress</b>: <code>${progressString}</code>\n<b>Size</b>: <code>${totalLengthStr}</code> | <b>Speed</b>: <code>${speedStr}ps</code> | <b>ETA</b>: <code>${eta}</code>\n<b>Seed By</b>:{user}`;
   var status = {
